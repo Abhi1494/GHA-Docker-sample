0611c0fdc48fbc9c41eb4ee89a583e27aa260f24
Abhi1494 <abhisheksinghnagwansi@gmail.com>
2024-07-02 00:14:28 +0530

    commiting GHA file with dockerfie

--- src/main/java/com/example/GHA_Docker_sample/GhaDockerSampleApplication.java
+++ src/main/java/com/example/GHA_Docker_sample/GhaDockerSampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GhaDockerSampleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GhaDockerSampleApplication.class, args);
 	}
 
