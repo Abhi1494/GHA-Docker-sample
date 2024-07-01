d58040b62d055273b20f3eb04d7fbd586988f5a2
Abhi1494 <abhisheksinghnagwansi@gmail.com>
2024-07-01 23:21:36 +0530

    first commit

---
+++ src/test/java/com/example/GHA_Docker_sample/GhaDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GHA_Docker_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaDockerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
